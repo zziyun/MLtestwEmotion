60ff783fb7f8c34b81ff57cb32dcc829e1812394
zziyun <jiyun7670@naver.com>
2020-04-29 21:20:33 +0900

    ml kit w emotion

---
+++ app/src/main/java/com/example/mlkit_happiness/FaceDetectionProcessor.java
@@ -0,0 +1,89 @@
+package com.example.mlkit_happiness;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.example.mlkit_happiness.utility.CameraImageGraphic;
+import com.example.mlkit_happiness.utility.FrameMetadata;
+import com.example.mlkit_happiness.utility.GraphicOverlay;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Face Detector Demo.
+ */
+public class FaceDetectionProcessor extends VisionProcessorBase<List<FirebaseVisionFace>> {
+
+    private static final String TAG = "FaceDetectionProcessor";
+
+    private final FirebaseVisionFaceDetector detector;
+
+    private final Bitmap overlayBitmap;
+
+    public FaceDetectionProcessor(Resources resources) {
+        FirebaseVisionFaceDetectorOptions options =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .build();
+
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+
+        overlayBitmap = BitmapFactory.decodeResource(resources, R.drawable.clown_nose);
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Face Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<List<FirebaseVisionFace>> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @Nullable Bitmap originalCameraImage,
+            @NonNull List<FirebaseVisionFace> faces,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+        graphicOverlay.clear();
+        if (originalCameraImage != null) {
+            CameraImageGraphic imageGraphic = new CameraImageGraphic(graphicOverlay, originalCameraImage);
+            graphicOverlay.add(imageGraphic);
+        }
+        for (int i = 0; i < faces.size(); ++i) {
+            FirebaseVisionFace face = faces.get(i);
+
+            int cameraFacing =
+                    frameMetadata != null ? frameMetadata.getCameraFacing() :
+                            Camera.CameraInfo.CAMERA_FACING_BACK;
+            FaceGraphic faceGraphic = new FaceGraphic(graphicOverlay, face, cameraFacing, overlayBitmap);
+            graphicOverlay.add(faceGraphic);
+        }
+        graphicOverlay.postInvalidate();
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Face detection failed " + e);
+    }
+}
